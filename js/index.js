7c254d2facaa9631f8fb4b14aa919f95e85deae5
Vitalii G_Samson <samson21.vgs@gmail.com>
2018-10-01 14:13:30 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -4,22 +4,23 @@ const b = parseFloat(prompt('Enter b'));
 const c = parseFloat(prompt('Enter c'));
 
 function solveQuadrEg(a, b, c) {
-    const discr = calsDiscr(a, b, c);
-if (discr > 0) {
-    const x1 = ((-b + Math.sqrt(discr))/(2 * a));
-    const x2 = ((-b - Math.sqrt(discr))/(2 * a)); 
-    return 'X1 = ' + x1 + 'X2 = ' +  x2;
-}
-else if (discr === 0) {
+    const discr = calDiscr(a, b, c);
+    if (discr > 0) {
+        const x1 = ((-b + Math.sqrt(discr)) / (2 * a));
+        const x2 = ((-b - Math.sqrt(discr)) / (2 * a));
+        return 'X1 = ' + x1 + 'X2 = ' + x2;
+    }
+    else if (discr === 0) {
         const x = (-b) / (2 * a);
         return x;
-}
+    }
     else if (discr < 0) {
-    return' No solution ';
+        return ' No solution ';
+    }
 }
-function calsDiscr(a, b, c) {
-      const discr = (b * b) - (4 * a * c);
-      return discr;
-}
-const solution = solveQardEg(a, b, c);
-alert(solution);
+    function calDiscr(a, b, c) {
+        const discr = (b * b) - (4 * a * c);
+        return discr;
+    }
+    const solution = solveQadrEg(a, b, c);
+    alert( solution);
