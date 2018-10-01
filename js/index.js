560d8526f992d197775f2a78d76dcb23d802565b
Vitalii G_Samson <samson21.vgs@gmail.com>
2018-10-01 11:34:04 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,28 @@
+alert('Трошки вправ');
+const a = parseFloat(prompt('Enter a'));
+const b = parseFloat(prompt('Enter b'));
+const c = parseFloat(prompt('Enter c'));
+
+function solveQardEg(a, b, c) {
+    const discr = calsDiscr(a, b, c);
+if (discr > 0) {
+    const x1 = ((-b + Math.sqrt(discr))/(2 * a));
+    const x2 = ((-b - Math.sqrt(discr))/(2 * a)); 
+    return 'X1 = ' + x1 + 'X2 = ' +  x2;
+}
+
+else if (discr === 0) {
+        const x = (-b) / (2 * a);
+        return x;
+    }
+    else if (discr < 0) {
+    return' No solution ';
+}
+
+function calsDiscr(a, b, c) {
+       const discr = (b * b) - (4 * a * c);
+       return discr;
+}
+
+const solution = solveQardEg(a, b, c);
+alert( solution );
